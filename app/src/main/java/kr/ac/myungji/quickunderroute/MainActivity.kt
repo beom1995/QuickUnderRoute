e8532b30e08f4fb8516a62ea2c53c5f8cdf0d429
LGgram-dndark95 <dihexyldecyl@gmail.com>
2021-10-09 20:12:51 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/myungji/quickunderroute/MainActivity.kt
@@ -0,0 +1,119 @@
+package kr.ac.myungji.quickunderroute
+
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.content.DialogInterface
+import android.graphics.Bitmap
+import android.net.http.SslError
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Message
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.*
+import android.widget.ProgressBar
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+
+        webView.apply {
+            webViewClient = WebViewClientClass() // 클릭시 새창 안뜨게
+
+            // WebView에서 별다른 설정없이도 크롬실행 가능하나 팝업 호출은 불가능함
+            // 가능하게 하기 위한 작업
+            webChromeClient = object : WebChromeClient() {
+                override fun onCreateWindow(view: WebView?, isDialog: Boolean, isUserGesture: Boolean, resultMsg: Message?): Boolean {
+                    val newWebView = WebView(this@MainActivity).apply {
+                        webViewClient = WebViewClient()
+                        settings.javaScriptEnabled = true
+                    }
+
+                    val dialog = Dialog(this@MainActivity).apply {
+                        setContentView(newWebView)
+                        window!!.attributes.width = ViewGroup.LayoutParams.MATCH_PARENT
+                        window!!.attributes.height = ViewGroup.LayoutParams.MATCH_PARENT
+                        show()
+                    }
+
+                    newWebView.webChromeClient = object : WebChromeClient() {
+                        override fun onCloseWindow(window: WebView?) {
+                            dialog.dismiss()
+                        }
+                    }
+
+                    (resultMsg?.obj as WebView.WebViewTransport).webView = newWebView
+                    resultMsg.sendToTarget()
+                    return true
+                }
+            }
+
+
+            //WebView 설정모음
+            settings.javaScriptEnabled = true   //자바스크립트실행가능
+            settings.setSupportMultipleWindows(true)    //새창생성허용
+            settings.javaScriptCanOpenWindowsAutomatically = true
+            settings.loadWithOverviewMode = true
+            settings.useWideViewPort = true
+            settings.setSupportZoom(true)
+            settings.builtInZoomControls = true
+            settings.cacheMode = WebSettings.LOAD_NO_CACHE
+            settings.domStorageEnabled = true
+            settings.displayZoomControls = true
+        }
+        webView.loadUrl("file:///android_asset/www/subwayMap.html")
+
+
+    }
+
+    //웹뷰에서 홈페이지를 띄웠을때 새창이 아닌 기존창에서 실행이 되도록 아래 코드를 넣어준다.
+    inner class WebViewClientClass : WebViewClient() {
+        //페이지 이동
+        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+            view.loadUrl(url)
+            return true
+        }
+/*
+        override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+            mProgressBar.visibility = ProgressBar.VISIBLE
+            webView.visibility = View.INVISIBLE
+        }
+
+        override fun onPageCommitVisible(view: WebView, url: String) {
+            super.onPageCommitVisible(view, url)
+            mProgressBar.visibility = ProgressBar.GONE
+            webView.visibility = View.VISIBLE
+        }
+*/
+
+        override fun onReceivedSslError(view: WebView, handler: SslErrorHandler, error: SslError) {
+            var builder: android.app.AlertDialog.Builder =
+                android.app.AlertDialog.Builder(this@MainActivity)
+            var message = "SSL Certificate error."
+            when (error.primaryError) {
+                SslError.SSL_UNTRUSTED -> message = "The certificate authority is not trusted."
+                SslError.SSL_EXPIRED -> message = "The certificate has expired."
+                SslError.SSL_IDMISMATCH -> message = "The certificate Hostname mismatch."
+                SslError.SSL_NOTYETVALID -> message = "The certificate is not yet valid."
+            }
+            message += " Do you want to continue anyway?"
+            builder.setTitle("SSL Certificate Error")
+            builder.setMessage(message)
+            builder.setPositiveButton("continue",
+                DialogInterface.OnClickListener { _, _ -> handler.proceed() })
+            builder.setNegativeButton("cancel",
+                DialogInterface.OnClickListener { dialog, which -> handler.cancel() })
+            val dialog: android.app.AlertDialog? = builder.create()
+            dialog?.show()
+        }
+    }
+
+
+}
